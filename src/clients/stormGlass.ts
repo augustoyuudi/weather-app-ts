032ebd43a3aa13e7cdb5ddcf3aa92ffa4394c679
augustoyuudi <augustoyuudi@gmail.com>
2021-12-06 23:06:47 -0300

    feat: handle StormGlass internal error

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -38,6 +38,13 @@ export class ClientRequestError extends InternalError {
   }
 }
 
+export class StormGlassResponseError extends InternalError {
+  constructor(message: string) {
+    const internalMessage = 'Unexpected error returned by the StormGlass service';
+    super(`${internalMessage}: ${message}`);
+  }
+}
+
 export class StormGlass {
   readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
 
@@ -54,6 +61,9 @@ export class StormGlass {
       return this.normalizeResponse(response.data);
 
     } catch (err: any) {
+      if (err.response && err.response.status) {
+        throw new StormGlassResponseError(`Error: ${JSON.stringify(err.response.data)} Code: ${err.response.status}`);
+      }
       throw new ClientRequestError(err.message);
     }
   }
