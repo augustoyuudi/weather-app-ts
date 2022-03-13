8b39598fe292c18e86384f3b9d6df642fd71691d
augustoyuudi <augustoyuudi@gmail.com>
2022-03-13 14:26:01 -0300

    feat: call logger in app main points

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach';
 import mongoose from 'mongoose';
 import { authMiddleware } from '@src/middlewares/auth';
+import logger from '@src/logger';
 
 @Controller('beaches')
 @ClassMiddleware(authMiddleware)
@@ -21,6 +22,7 @@ export class BeachesController {
         return;
       }
 
+      logger.error(error);
       res.status(500).send({ error: 'Internal server error'});
     }
   }
